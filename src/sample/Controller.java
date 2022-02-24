72bcee96e04df4f5ecf9d0503417c96ba9fad08c
Лера <leravodopyanova@gmail.com>
2022-02-24 22:29:02 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    public TextField numOfSides;
+    public MenuButton menuLogic;
+    public Canvas mainCanvas;
+    public TextField heightTextField;
+    public TextField widthTextField;
+    public ColorPicker colorPicker;
+    public Label figureName;
+
+    public void canvasMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void wMouseClick(MouseEvent mouseEvent) {
+    }
+}
